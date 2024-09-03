bb1478bff49ef3da065653026d68f0c3947e7f56
while1win <yaman.mouwakeh@le-campus-numerique.fr>
2024-09-03 10:05:37 +0200

    exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
 // you can write js here
 
-console.log('exo-5');
+//console.log('exo-5');
+let input = 'Turpentine and turtles';
+let vowels =[ 'a', 'e', 'i', 'o', 'u', 'y', 'h'];
+let resultArray = [];
+
+for ( var i = 0; i<input.length; i++){
+    console.log(input[i].toLocaleLowerCase());
+    for (var j = 0; j<vowels.length; j++){
+        if ( input[i] === vowels[j]){
+            resultArray.push(input[i]);
+            break;
+        }
+    }
+}
+console.log(resultArray.join('').toUpperCase());
